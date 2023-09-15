30a8f1882c7c1afb551177ac13e26783c4aa640e
Luis <LuisDEsquivel11@gmail.com>
2023-09-14 19:24:35 -0500

    part two of exercise completed

--- src/main/java/edu/codeup/codeupspringblog/controllers/MathController.java
+++ src/main/java/edu/codeup/codeupspringblog/controllers/MathController.java
@@ -2,10 +2,37 @@ package edu.codeup.codeupspringblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MathController {
 
-    @GetMapping("/add")
-    public String add(int num)
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String add(@PathVariable int num1, @PathVariable int num2) {
+        int sum = num1 + num2;
+        return String.format("%s plus %s equals %s", num1, num2, sum);
+    }
+
+    @GetMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public String subtract(@PathVariable int num1, @PathVariable int num2) {
+        int difference = num1 - num2;
+        return String.format("%s minus %s equals %s", num1, num2, difference);
+    }
+
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String multiply(@PathVariable int num1, @PathVariable int num2) {
+        int product = num1 * num2;
+        return String.format("%s times %s equals %s", num1, num2, product);
+    }
+
+    @GetMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public String divide(@PathVariable int num1, @PathVariable int num2) {
+        int quotient = num1 / num2;
+        return String.format("%s divided by %s equals %s", num1, num2, quotient);
+    }
 }
